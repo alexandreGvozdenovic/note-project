09e8269c027cd1a2a8f1bcd45a74405bf20e390c
Alexandre Gvozdenovic <alexandregvozdenovic@revolugo.com>
2024-03-21 09:39:28 +0100

    creation logic is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,78 @@
-import { Container } from 'react-bootstrap'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import { NewNote } from './NewNote'
- 
+import { Container } from "react-bootstrap";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { NewNote } from "./NewNote";
+import { useLocalStorage } from "./useLocalStorage";
+import { useMemo } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+export type Note = {
+  id: string;
+} & NoteData;
+export type RawNote = {
+  id: string;
+} & RawNoteData;
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
+export type Tag = {
+  id: string;
+  label: string;
+};
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
 
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
 
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes((prevNotes) => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuidv4(), tagIds: tags.map((tag) => tag.id) },
+      ];
+    });
+  }
+
+  function addTag(tag: Tag) {
+    setTags((prev) => [...prev, tag]);
+  }
   return (
-    <Container className='my-4'>
+    <Container className="my-4">
       <Routes>
-        <Route path='/' element={<h1>Home</h1>}></Route>
-        <Route path='/new' element={<NewNote />}></Route>
-        <Route path='/:id'>
+        <Route path="/" element={<h1>Home</h1>}></Route>
+        <Route
+          path="/new"
+          element={
+            <NewNote
+              onSubmit={onCreateNote}
+              onAddTag={addTag}
+              availableTags={tags}
+            />
+          }
+        ></Route>
+        <Route path="/:id">
           <Route index element={<h1>show</h1>}></Route>
-          <Route path='edit' element={<h1>edit</h1>}></Route>
+          <Route path="edit" element={<h1>edit</h1>}></Route>
         </Route>
-        <Route path='*' element={<Navigate to="/" />}></Route>
+        <Route path="*" element={<Navigate to="/" />}></Route>
       </Routes>
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,7 +1,19 @@
-export function NewNote() {
-    return (
-        <>
-            <h1>Hi</h1>
-        </>
-    )
+import { NoteData, Tag } from "./App";
+import { NoteForm } from "./NoteForm";
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
+};
+export function NewNote({ onSubmit, onAddTag, availableTags }: NewNoteProps) {
+  return (
+    <>
+      <h1 className="mb-4">New Note</h1>
+      <NoteForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
+    </>
+  );
 }
